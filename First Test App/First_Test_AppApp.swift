68d82be7ad0459dafb12c0aee8e614cf3285b350
AMP-808 <62463401+AMP-808@users.noreply.github.com>
2021-02-21 21:57:29 -1000

    Initial Commit

---
+++ First Test App/First_Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_Test_AppApp.swift
+//  First Test App
+//
+//  Created by Cristian Torres on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct First_Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
